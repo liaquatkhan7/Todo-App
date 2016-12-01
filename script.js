bd2eef47f3d849fde848139dbae5eb093485fb26
Liaquat Ali Khan <farrukh_khan16@hotmail.com>
2016-11-30 18:30:44 -0800

    Edited script file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-alert("Welcome");
+var fullArr = [];
+var val;
+var cros = "<img src='1480124680_cancel.png' alt='cancel' width='15px' height='15px'>"
+function add(){
+    val = document.getElementById("textFiled").value;
+    fullArr.push(val);
+    document.getElementById("todoHere").innerHTML += "<li>"+ val + cros + "</li>" ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
